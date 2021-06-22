cb83a63f4eba0102e8788a2e73e1a51f57a59057
张晋明 <245545357@qq.com>
2021-06-22 20:28:58 +0800

    新增GodDrawable databing方案,无需多余的xml即可实现各种drawable效果

--- app/src/main/java/com/mc/cpyr/basekittest/MainActivity.kt
+++ app/src/main/java/com/mc/cpyr/basekittest/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.mc.cpyr.basekittest
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.kit.base.activity.DataBindingActivity
 import com.kit.base.viewmodel.BaseViewModel
 import com.mc.cpyr.basekittest.databinding.ActivityMainBinding
 import com.mc.cpyr.kit.ext.logI
 import com.mm.ext.IE
-import com.skt.lib.helper.startFragment
+import com.skt.lib.ui.view.GOrientation
 
 class MainActivity : DataBindingActivity<ActivityMainBinding, BaseViewModel>() {
 
@@ -13,16 +15,43 @@ class MainActivity : DataBindingActivity<ActivityMainBinding, BaseViewModel>() {
         return R.layout.activity_main
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun initLayout() {
         super.initLayout()
 
-        mBinding.btn.setOnClickListener {
-            startFragment("/app/CounterFragment")
+        mBinding.cardView.setOnClickListener {
+//            startFragment("/app/CounterFragment")
 //            startTitleFragment("/app/CounterFragment")
             val s = true.IE({ "111" }, { 1 })
             logI { s }
         }
 
+
+//        val view:GradientDrawable
+//        view.cornerRadii= floatArrayOf(1f,1f)
+
+        test(-1)
+
+//        val drawable= DelegateDrawable()
+//        val norDrawable= ColorDrawable()
+//        norDrawable.color = getColor(R.color.purple_700)
+//        val pressDrawable=ColorDrawable()
+//        pressDrawable.color = getColor(R.color.purple_200)
+//
+//        val pressDrawable2=ColorDrawable()
+//        pressDrawable2.color = getColor(R.color.teal_200)
+//
+////        drawable.addState(intArrayOf(-android.R.attr.state_pressed),norDrawable)
+////        drawable.addState(intArrayOf(android.R.attr.state_pressed),pressDrawable)
+//        drawable.addState(intArrayOf(android.R.attr.state_enabled),pressDrawable)
+//
+//        drawable.addState(intArrayOf(0),norDrawable)
+//        mBinding.cardView.backgroundDrawable=drawable
+//        mBinding.cardView.isEnabled=false
+
+
+    }
+    fun test(@GOrientation int: Int){
 
     }
 
@@ -31,13 +60,12 @@ class MainActivity : DataBindingActivity<ActivityMainBinding, BaseViewModel>() {
     }
 
     override fun initView() {
-        TODO("Not yet implemented")
+
     }
 
     override fun initViewData() {
-        TODO("Not yet implemented")
-    }
 
+    }
 
 }
 
